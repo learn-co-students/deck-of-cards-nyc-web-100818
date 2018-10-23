16163f4a54a90a51ff7f544ce7af508554ec110e
github username <github email address>
2018-10-23 10:41:49 -0400

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,39 @@
 class Deck
+    attr_accessor :deck
 
+    VALUES = %w[A 2 3 4 5 6 7 8 9 10 J Q K].freeze
+
+    # SUITS = { ":spades:" => "spades", ":diamonds:", ":clubs:", ":hearts:" => "hearts" }
+    SUITS = %w[hearts diamonds clubs spades].freeze
+
+    def self.all_cards
+        deck = []
+        VALUES.each do |val|
+            SUITS.each do |suit|
+                deck << Card.new(suit, val)
+            end
+        end
+        deck
+    end
+
+    def initialize(deck = Deck.all_cards)
+        @deck = deck
+    end
+
+    def choose_card
+        @deck.shuffle!.pop
+    end
+
+    def cards
+        @deck
+    end
 end
 
 class Card
+    attr_accessor :suit, :rank
 
+    def initialize(suit, rank)
+        @suit = suit
+        @rank = rank
+    end
 end
