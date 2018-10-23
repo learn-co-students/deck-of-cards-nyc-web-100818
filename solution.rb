ecad56d85f3d45604df818ff9607e42612ebcf17
Rexman17 <a.stone1794@gmail.com>
2018-10-23 10:42:47 -0400

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,48 @@
 class Deck
+  attr_accessor :cards, :phrase
+  attr_reader :ranks, :suits
 
-end
+  def initialize
+    @cards = []
+    @phrases = []
+    @ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    @suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+    @rank_spelled_out = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+
+    create_card(suits, ranks)
+
+  end # end of initialize
+
+  def create_card(array1, array2)
+    @ranks.each do |rank|
+      @suits.each do |suit|
+        @cards << Card.new(suit, rank)
+      end
+    end
+  end
+
+  def choose_card
+    @cards.shuffle # so it's random
+    @cards.pop
+  end
+
+
+end # end of Deck class
 
 class Card
+    attr_accessor :rank, :suit
 
-end
+    @@all = []
+
+    def initialize(suit, rank) # card instance is made up of a rank and a suit
+      @rank = rank
+      @suit = suit
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+
+end # end of Card class
